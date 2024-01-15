c41f2b1c190f97009dae635f11109ef09e3ddb23
w1th4d <119284044+w1th4d@users.noreply.github.com>
2024-01-16 00:18:55 +0800

    Add library class

---
+++ target-app/src/main/java/org/example/target/Lib.java
@@ -0,0 +1,15 @@
+package org.example.target;
+
+public class Lib {
+    public Lib() {
+        System.out.println("This is an instance of the target library.");
+    }
+
+//    private static void benign() {
+//    }
+//
+//    static {
+//        System.out.println("This is a static block in the target library.");
+//        benign();
+//    }
+}
--- target-app/src/main/java/org/example/target/Main.java
+++ target-app/src/main/java/org/example/target/Main.java
@@ -2,6 +2,7 @@ package org.example.target;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is the target app.");
+        System.out.println("This is main of the target app.");
+        new Lib();
     }
 }

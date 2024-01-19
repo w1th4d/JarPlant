03395113e5720ea7c497fb4f714a3eb3a052a49f
w1th4d <119284044+w1th4d@users.noreply.github.com>
2024-01-19 17:48:02 +0800

    Try all Spring annotations

--- target-app-spring-boot/src/main/java/com/example/restservice/RestServiceApplication.java
+++ target-app-spring-boot/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,13 +1,17 @@
 package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+// The @SpringBootApplication annotation is a shorthand for @Configuration, @EnableAutoConfiguration and @ComponentScan
+// If used, this will show up as its own annotation in the bytecode instead of these three
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan      // This annotation is responsible for searching the package for any @RestController annotations
 public class RestServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
     }
-
 }

96049455d09c192f24256fffd182287215b5768c
w1th4d <119284044+w1th4d@users.noreply.github.com>
2024-04-26 13:16:29 +0800

    Add support for finding a test implant class This is a simplified version from the ImplantHandlerImpl but for the mock.

--- jarplant-lib/src/test/java/org/example/injector/ImplantHandlerMock.java
+++ jarplant-lib/src/test/java/org/example/injector/ImplantHandlerMock.java
@@ -2,7 +2,12 @@ package org.example.injector;
 
 import javassist.bytecode.ClassFile;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.CodeSource;
 import java.util.Map;
 
 public class ImplantHandlerMock implements ImplantHandler {
@@ -10,6 +15,31 @@ public class ImplantHandlerMock implements ImplantHandler {
 
     ImplantHandlerMock(ClassFile specimen) {
         this.specimen = specimen;
+    }
+
+    public static ImplantHandler findAndCreateFor(final Class<?> clazz) throws ClassNotFoundException, IOException {
+        CodeSource codeSource = clazz.getProtectionDomain().getCodeSource();
+        if (codeSource == null) {
+            // Can't find oneself
+            throw new ClassNotFoundException("Can't determine the path to the class file");
+        }
+        Path sourcePath = Path.of(codeSource.getLocation().getPath());
+
+        if (!Files.isDirectory(sourcePath)) {
+            throw new RuntimeException("Expected a directory.");
+        }
+
+        // Convert "org.example.injector.Inject" to "org/example/injector/Inject.class"
+        String[] packageHierarchy = clazz.getName().split("\\.");
+        packageHierarchy[packageHierarchy.length - 1] += ".class";
+        Path sourceFullPath = Path.of(sourcePath.toString(), packageHierarchy);
+
+        if (!Files.exists(sourcePath)) {
+            throw new ClassNotFoundException(sourcePath.toString());
+        }
+
+        ClassFile cf = new ClassFile(new DataInputStream(new FileInputStream(sourceFullPath.toFile())));
+        return new ImplantHandlerMock(cf);
     }
 
     @Override

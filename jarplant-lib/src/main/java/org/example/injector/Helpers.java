a65836a03d052386bd5217a1cb666b6d409740fc
w1th4d <119284044+w1th4d@users.noreply.github.com>
2024-08-02 00:56:47 +0200

    Use BufferedJarFiddler instead of StreamedJarFiddler

--- jarplant-lib/src/main/java/org/example/injector/Helpers.java
+++ jarplant-lib/src/main/java/org/example/injector/Helpers.java
@@ -154,12 +154,11 @@ public class Helpers {
     public static boolean jarLooksSigned(Path jarFilePath) throws IOException {
         Pattern regex = Pattern.compile("META-INF/.+\\.SF|DSA|RSA");
 
-        try (StreamedJarFiddler jar = StreamedJarFiddler.open(jarFilePath)) {
-            for (StreamedJarFiddler.StreamedJarEntry entry : jar) {
-                Matcher matcher = regex.matcher(entry.getName());
-                if (matcher.matches()) {
-                    return true;
-                }
+        BufferedJarFiddler jar = BufferedJarFiddler.read(jarFilePath);
+        for (BufferedJarFiddler.BufferedJarEntry entry : jar) {
+            Matcher matcher = regex.matcher(entry.getName());
+            if (matcher.matches()) {
+                return true;
             }
         }
 

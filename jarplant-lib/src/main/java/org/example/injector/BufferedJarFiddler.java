60b92c2072a647e569f61ba87bf16a621977a231
w1th4d <119284044+w1th4d@users.noreply.github.com>
2024-08-02 00:22:21 +0200

    Add BufferedJarFiddler This is a re-write of StreamedJarFiddler (aka the original JarFileFiddler). This one fully reads and buffers all JAR content in memory. The downside is that it demands a lot of more RAM. The upside is that the code is a lot simpler and that it can overwrite the same file on disk (without having to write it to another output/temporary file).

---
+++ jarplant-lib/src/main/java/org/example/injector/BufferedJarFiddler.java
@@ -0,0 +1,241 @@
+package org.example.injector;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipException;
+
+/**
+ * Utility for modifying contents of a JAR file.
+ * <p>This class will read the entire contents of a JAR file and buffer it into memory.
+ * Use the iterator to go through all entries and replace its content as appropriate.</p>
+ * <p>The iterator is thread safe as it's backed by a <copy>CopyOnWriteArrayList</copy>, but
+ * modifying the content of an entry is not (by design).
+ * This class is not designed to be used by multithreaded code.</p>
+ * <p>All methods of this class will operate upon the buffered state in memory.
+ * Use the <code>write</code> methods to persist the state to disk (or elsewhere).</p>
+ * <p>Example:</p>
+ * <code>
+ * BufferedJarFiddler fiddler = BufferedJarFiddler.read(jarFilePath);
+ * for (BufferedJarFiddler.BufferedJarEntry entry : fiddler) {
+ * if (isInteresting(entry)) {
+ * entry.replaceContentWith(somethingElse);
+ * }
+ * }
+ * subject.write(jarFilePath);
+ * </code>
+ */
+public class BufferedJarFiddler implements Iterable<BufferedJarFiddler.BufferedJarEntry> {
+    private final List<BufferedJarEntry> entries;
+    private final Set<String> cachedEntryNames;
+
+    BufferedJarFiddler(List<BufferedJarEntry> entries) {
+        this.entries = new CopyOnWriteArrayList<>(entries);
+        this.cachedEntryNames = new HashSet<>(entries.size());
+        for (BufferedJarEntry entry : this.entries) {
+            this.cachedEntryNames.add(entry.getName());
+        }
+    }
+
+    /**
+     * Read the entire content of a JAR file into memory.
+     *
+     * @param jarFilePath Path to existing JAR file
+     * @return Instance
+     * @throws IOException If unable to read file
+     */
+    public static BufferedJarFiddler read(Path jarFilePath) throws IOException {
+        List<BufferedJarEntry> readEntries = new ArrayList<>();
+        try (JarFile jar = new JarFile(jarFilePath.toFile())) {
+            Enumeration<JarEntry> entries = jar.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                byte[] content = jar.getInputStream(entry).readAllBytes();
+                readEntries.add(new BufferedJarEntry(entry, content));
+            }
+        }
+        return new BufferedJarFiddler(readEntries);
+    }
+
+    /**
+     * Write the current state to a file.
+     * The output file can be the same as the input file.
+     *
+     * @param outputFile Path to writable file (will be created if not exist)
+     * @throws IOException If unable to create or write file
+     */
+    public void write(Path outputFile) throws IOException {
+        try (OutputStream outputStream = Files.newOutputStream(outputFile)) {
+            write(outputStream);
+        }
+    }
+
+    /**
+     * Write the current state to a stream.
+     *
+     * @param outputStream An open output stream
+     * @throws IOException If failed to write
+     */
+    public void write(OutputStream outputStream) throws IOException {
+        try (JarOutputStream jarOutputStream = new JarOutputStream(outputStream)) {
+            for (BufferedJarEntry entry : entries) {
+                jarOutputStream.putNextEntry(entry.metadata);
+                jarOutputStream.write(entry.content);
+                jarOutputStream.closeEntry();
+            }
+        }
+    }
+
+    /**
+     * Add a new entry at the end of the JAR.
+     *
+     * @param newEntry Entry to add
+     * @param contents Entry file content
+     * @throws ZipException If the entry name already exist
+     */
+    public void addNewEntry(JarEntry newEntry, byte[] contents) throws ZipException {
+        if (cachedEntryNames.contains(newEntry.getName())) {
+            throw new ZipException("Entry " + newEntry.getName() + " already exist.");
+        }
+
+        entries.add(new BufferedJarEntry(newEntry, contents));
+        cachedEntryNames.add(newEntry.getName());
+    }
+
+    /**
+     * List all entries found in the JAR.
+     *
+     * @return File names
+     */
+    public List<String> listEntries() {
+        List<String> results = new ArrayList<>(entries.size());
+        for (BufferedJarEntry entry : entries) {
+            results.add(entry.metadata.getName());
+        }
+        return Collections.unmodifiableList(results);
+    }
+
+    /**
+     * Get a specific entry (file) from the JAR.
+     *
+     * @param path Full internal path in the JAR. Ex: /META-INF/MANIFEST.MF
+     * @return Entry, if it was found
+     */
+    public Optional<BufferedJarEntry> getEntry(String path) {
+        for (BufferedJarEntry entry : entries) {
+            if (entry.metadata.getName().equals(path)) {
+                return Optional.of(entry);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Get all entries found in the JAR.
+     * Returned objects are mutable. Any changes to these objects will stick.
+     *
+     * @return Entries
+     */
+    public List<BufferedJarEntry> getEntries() {
+        return entries;
+    }
+
+    @Override
+    public ListIterator<BufferedJarEntry> iterator() {
+        return entries.listIterator();
+    }
+
+    /**
+     * Holds both the JarEntry (metadata) and actual file contents.
+     * This is suprizingly revolutionary in comparison to the native API for handling JAR files.
+     */
+    public static class BufferedJarEntry {
+        private final JarEntry metadata;
+        private byte[] content;
+
+        BufferedJarEntry(JarEntry metadata, byte[] content) {
+            this.metadata = metadata;
+            this.content = content;
+        }
+
+        /**
+         * Get the full file name of this entry.
+         */
+        public String getName() {
+            return metadata.getName();
+        }
+
+        /**
+         * Get a clone of the underlying JarEntry.
+         *
+         * @return JarEntry
+         */
+        public JarEntry toJarEntry() {
+            return (JarEntry) metadata.clone();
+        }
+
+        /**
+         * Get the file content of this entry.
+         *
+         * @return Bytes
+         */
+        public byte[] getContent() {
+            return content;
+        }
+
+        /**
+         * Get the file contents of this entry.
+         *
+         * @return An open stream
+         */
+        public InputStream getContentStream() {
+            return new ByteArrayInputStream(content);
+        }
+
+        /**
+         * Replace the file contents with new data.
+         *
+         * @param newContent Data that will replace the content
+         */
+        public void replaceContentWith(byte[] newContent) {
+            this.content = newContent;
+        }
+
+        /**
+         * Replace the file contents with new data.
+         *
+         * @param content ByteBuffer that is ready to read
+         */
+        public void replaceContentWith(ByteBuffer content) {
+            byte[] bytes = new byte[content.remaining()];
+            content.get(bytes);
+            replaceContentWith(bytes);
+        }
+
+        /**
+         * Replace the file contents with new data.
+         * The input stream will be read until EOF.
+         *
+         * @param in An open InputStream
+         * @throws IOException If failed to read the stream
+         */
+        public void replaceContentWith(InputStream in) throws IOException {
+            byte[] bytes = in.readAllBytes();
+            replaceContentWith(bytes);
+        }
+
+        @Override
+        public String toString() {
+            return metadata.getName();
+        }
+    }
+}
---
+++ jarplant-lib/src/test/java/org/example/injector/BufferedJarFiddlerTests.java
@@ -0,0 +1,295 @@
+package org.example.injector;
+
+import javassist.bytecode.ClassFile;
+import javassist.bytecode.DuplicateMemberException;
+import javassist.bytecode.FieldInfo;
+import org.example.TestHelpers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+import java.util.zip.CRC32;
+import java.util.zip.ZipEntry;
+
+import static org.junit.Assert.*;
+
+public class BufferedJarFiddlerTests {
+    private Path testJar;
+    private Path outputJar;
+    private final Set<String> expectedEntries = Set.of(
+            "META-INF/",
+            "META-INF/MANIFEST.MF",
+            "org/",
+            "org/example/",
+            "org/example/target/",
+            "META-INF/maven/",
+            "META-INF/maven/org.example/",
+            "META-INF/maven/org.example/test-app-pojo/",
+            "org/example/target/Main.class",
+            "org/example/target/Lib.class",
+            "META-INF/maven/org.example/test-app-pojo/pom.xml",
+            "META-INF/maven/org.example/test-app-pojo/pom.properties"
+    );
+
+    @Before
+    public void getTestJar() throws IOException {
+        Path testEnv = TestHelpers.findTestEnvironmentDir(this.getClass());
+        Path testJarPath = testEnv.resolve("test-app-pojo-with-debug.jar");
+        if (Files.notExists(testJarPath)) {
+            throw new RuntimeException("Cannot find test JAR: " + testJarPath + ". Have you tried running `mvn package` (at the parent level)?");
+        }
+
+        Path tempFile = Files.createTempFile("test-app-", UUID.randomUUID() + ".jar");
+        Files.write(tempFile, Files.readAllBytes(testJarPath));
+
+        this.testJar = tempFile;
+    }
+
+    @Before
+    public void createOutputJar() throws IOException {
+        outputJar = Files.createTempFile("BufferedJarFiddlerTests-", UUID.randomUUID() + ".jar");
+    }
+
+    @After
+    public void removeOutputJar() throws IOException {
+        Files.delete(outputJar);
+    }
+
+    @Test
+    public void testIterator_OnlyReadWholeJar_FoundAllClasses() throws IOException {
+        // Arrange
+        Set<String> entriesFoundInJar = new HashSet<>(expectedEntries.size());
+
+        // Act
+        BufferedJarFiddler subject = BufferedJarFiddler.read(testJar);
+        for (BufferedJarFiddler.BufferedJarEntry entry : subject) {
+            entriesFoundInJar.add(entry.getName());
+        }
+
+        // Assert
+        assertEquals("Found all files in JAR.", expectedEntries, entriesFoundInJar);
+    }
+
+    @Test
+    public void testIterator_CopyWholeJar_AllClassesInOutput() throws IOException {
+        // Arrange
+        Set<String> entriesFoundInJar = new HashSet<>(expectedEntries.size());
+
+        // Act
+        BufferedJarFiddler subject = BufferedJarFiddler.read(testJar);
+        for (BufferedJarFiddler.BufferedJarEntry entry : subject) {
+            entriesFoundInJar.add(entry.getName());
+        }
+        subject.write(outputJar);
+
+        // Assert
+        Set<String> entriesInOutputJar = readAllJarEntries(outputJar).stream()
+                .map(ZipEntry::getName)
+                .collect(Collectors.toSet());
+        assertEquals("All files were forwarded to output JAR.", entriesInOutputJar, entriesFoundInJar);
+    }
+
+    @Test
+    @Ignore // TODO Something makes JarOutputStream.putNextEntry() add to the extra field (causing a diff).
+    public void testIterator_CopyWholeJar_CopiedEntriesMetadata() throws IOException {
+        // Arrange
+        Set<JarEntry> entriesFoundInJar = new HashSet<>(expectedEntries.size());
+
+        // Act
+        BufferedJarFiddler subject = BufferedJarFiddler.read(testJar);
+        for (BufferedJarFiddler.BufferedJarEntry entry : subject) {
+            entriesFoundInJar.add(entry.toJarEntry());
+        }
+        subject.write(outputJar);
+
+        // Assert: Make sure all metadata is intact
+        try (JarFile outputJarFile = new JarFile(outputJar.toFile())) {
+            for (JarEntry entryFoundInJar : entriesFoundInJar) {
+                JarEntry correspondingOutputEntry = (JarEntry) outputJarFile.getEntry(entryFoundInJar.getName());
+                assertAllJarEntryMetadataEquals(entryFoundInJar, correspondingOutputEntry);
+            }
+        }
+    }
+
+    @Test
+    public void testIterator_ModifyEntry_OnlyEntryModified() throws IOException {
+        // Arrange
+        String nameOfMain = "org/example/target/Main.class";
+        HashMap<String, Long> originalFileHashes = new HashMap<>();
+
+        // Act: Go through the entire JAR and modify Main.class
+        BufferedJarFiddler subject = BufferedJarFiddler.read(testJar);
+        for (BufferedJarFiddler.BufferedJarEntry entry : subject) {
+            originalFileHashes.put(entry.getName(), entry.toJarEntry().getCrc());
+
+            if (entry.getName().equals(nameOfMain)) {
+                ClassFile mainClass = new ClassFile(new DataInputStream(entry.getContentStream()));
+                try {
+                    mainClass.addField(new FieldInfo(mainClass.getConstPool(), "ADDED_FIELD", "I"));
+                } catch (DuplicateMemberException e) {
+                    throw new RuntimeException(e);
+                }
+
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                mainClass.write(new DataOutputStream(buffer));
+                entry.replaceContentWith(buffer.toByteArray()); // Actual act
+            }
+        }
+        subject.write(outputJar);
+
+        // Assert: Make sure that only Main.class is modified (but not the other entries)
+        for (JarEntry entry : readAllJarEntries(outputJar)) {
+            long originalHash = originalFileHashes.get(entry.getName());
+            long foundHash = entry.getCrc();
+            if (entry.getName().equals(nameOfMain)) {
+                assertNotEquals("Modified entry CRC differs.", originalHash, foundHash);
+            } else {
+                assertEquals("Unmodified entry CRC matches.", originalHash, foundHash);
+            }
+        }
+    }
+
+    @Test
+    public void testEntryReplacement_ReplaceEntryFromByteArray_EntryReplaced() throws IOException {
+        // Arrange
+        String nameOfMain = "org/example/target/Main.class";
+        byte[] replacementData = new byte[]{1, 2, 3, 4, 5};
+
+        // Act
+        BufferedJarFiddler subject = BufferedJarFiddler.read(testJar);
+        subject.getEntry(nameOfMain).orElseThrow().replaceContentWith(replacementData);
+        subject.write(outputJar);
+
+        // Assert
+        try (JarFile output = new JarFile(outputJar.toFile())) {
+            ZipEntry mainEntry = output.getEntry(nameOfMain);
+            assertNotNull("Entry exist.", mainEntry);
+
+            long originalHash = crc32(ByteBuffer.wrap(replacementData));
+            long foundHash = mainEntry.getCrc();
+            assertEquals("Entry CRC is modified.", originalHash, foundHash);
+
+            byte[] mainEntryContent = output.getInputStream(mainEntry).readAllBytes();
+            assertNotEquals("Actual content differ.", replacementData, mainEntryContent);
+        }
+    }
+
+    @Test
+    public void testEntryReplacement_ReplaceEntryFromBuffer_EntryReplaced() throws IOException {
+        // Arrange
+        String nameOfMain = "org/example/target/Main.class";
+        ByteBuffer replacementData = ByteBuffer.wrap(new byte[]{1, 2, 3, 4, 5});
+
+        // Act
+        BufferedJarFiddler subject = BufferedJarFiddler.read(testJar);
+        subject.getEntry(nameOfMain).orElseThrow().replaceContentWith(replacementData);
+        subject.write(outputJar);
+
+        // Assert
+        try (JarFile output = new JarFile(outputJar.toFile())) {
+            ZipEntry mainEntry = output.getEntry(nameOfMain);
+            assertNotNull("Entry exist.", mainEntry);
+
+            long originalHash = crc32(replacementData.rewind());
+            long foundHash = mainEntry.getCrc();
+            assertEquals("Entry CRC is modified.", originalHash, foundHash);
+
+            ByteBuffer mainEntryContent = ByteBuffer.wrap(output.getInputStream(mainEntry).readAllBytes());
+            assertNotEquals("Actual content differ.", replacementData, mainEntryContent);
+        }
+    }
+
+    @Test
+    public void testEntryReplacement_ReplaceEntryFromStream_EntryReplaced() throws IOException {
+        // Arrange
+        String nameOfMain = "org/example/target/Main.class";
+        byte[] replacementData = new byte[]{1, 2, 3, 4, 5};
+        InputStream replacementStream = new ByteArrayInputStream(replacementData);
+
+        // Act
+        BufferedJarFiddler subject = BufferedJarFiddler.read(testJar);
+        subject.getEntry(nameOfMain).orElseThrow().replaceContentWith(replacementStream);
+        subject.write(outputJar);
+
+        // Assert
+        try (JarFile output = new JarFile(outputJar.toFile())) {
+            ZipEntry mainEntry = output.getEntry(nameOfMain);
+            assertNotNull("Entry exist.", mainEntry);
+
+            long originalHash = crc32(ByteBuffer.wrap(replacementData));
+            long foundHash = mainEntry.getCrc();
+            assertEquals("Entry CRC is modified.", originalHash, foundHash);
+
+            byte[] mainEntryContent = output.getInputStream(mainEntry).readAllBytes();
+            assertNotEquals("Actual content differ.", replacementData, mainEntryContent);
+        }
+    }
+
+    @Test
+    public void testWrite_OpenStream_StreamLeftOpen() throws IOException {
+        // Arrange
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        // Act
+        BufferedJarFiddler.read(testJar).write(buffer);
+
+        // Assert
+        try {
+            buffer.write(new byte[]{1, 2, 3});
+        } catch (IOException e) {
+            fail("Unable to continue writing to stream (stream closed).");
+        }
+    }
+
+    @Test
+    public void testWrite_SameFile_Fine() throws IOException {
+        // Act + Assert
+        BufferedJarFiddler.read(testJar).write(testJar);
+    }
+
+    private static Set<JarEntry> readAllJarEntries(Path jarFile) {
+        Set<JarEntry> results = new HashSet<>();
+
+        try (JarFile jar = new JarFile(jarFile.toFile())) {
+            Enumeration<JarEntry> entries = jar.entries();
+            while (entries.hasMoreElements()) {
+                results.add(entries.nextElement());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open JAR file used for testing.", e);
+        }
+
+        return results;
+    }
+
+    private static long crc32(ByteBuffer buffer) {
+        CRC32 sum = new CRC32();
+        sum.update(buffer);
+        return sum.getValue();
+    }
+
+    private static void assertAllJarEntryMetadataEquals(JarEntry a, JarEntry b) {
+        assertEquals(a.getName(), b.getName());
+        assertEquals(a.getSize(), b.getSize());
+        assertEquals(a.getCompressedSize(), b.getCompressedSize());
+        assertEquals(a.getCrc(), b.getCrc());
+        assertEquals(a.getCreationTime(), b.getCreationTime());
+        assertEquals(a.getLastModifiedTime(), b.getLastModifiedTime());
+        assertEquals(a.getLastAccessTime(), b.getLastAccessTime());
+        assertEquals(a.getTime(), b.getTime());
+        assertEquals(a.getTimeLocal(), b.getTimeLocal());
+        assertEquals(a.getComment(), b.getComment());
+        assertArrayEquals(a.getExtra(), b.getExtra());
+        assertArrayEquals(a.getCodeSigners(), b.getCodeSigners());
+        assertArrayEquals(a.getCertificates(), b.getCertificates());
+    }
+}

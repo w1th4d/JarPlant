0a7e3811ed7e7d8cf4411bb18959c9492db74447
w1th4d <119284044+w1th4d@users.noreply.github.com>
2025-03-28 20:46:06 +0100

    Copy ClassImplant template as new RevShellImplant

---
+++ jarplant-implants/src/main/java/io/github/w1th4d/jarplant/implants/RevShellImplant.java
@@ -0,0 +1,40 @@
+package io.github.w1th4d.jarplant.implants;
+
+public class RevShellImplant implements Runnable, Thread.UncaughtExceptionHandler {
+    static volatile String CONF_JVM_MARKER_PROP = "java.class.init";
+    static volatile boolean CONF_BLOCK_JVM_SHUTDOWN = false;
+    static volatile int CONF_DELAY_MS = 0;
+
+    @SuppressWarnings("unused")
+    public static void init() {
+        if (System.getProperty(CONF_JVM_MARKER_PROP) == null) {
+            if (System.setProperty(CONF_JVM_MARKER_PROP, "true") == null) {
+                RevShellImplant implant = new RevShellImplant();
+                Thread background = new Thread(implant);
+                background.setDaemon(!CONF_BLOCK_JVM_SHUTDOWN);
+                background.setUncaughtExceptionHandler(implant);
+                background.start();
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        if (CONF_DELAY_MS > 0) {
+            try {
+                Thread.sleep(CONF_DELAY_MS);
+            } catch (InterruptedException ignored) {
+            }
+        }
+
+        payload();
+    }
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable throwable) {
+        // Silently ignore (don't throw up error messages on stderr)
+    }
+
+    private void payload() {
+    }
+}
